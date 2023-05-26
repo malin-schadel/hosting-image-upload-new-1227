7076144da7892e78231baa830f140f658dae6f1f
Malin Schadel <malin.schadel@gmail.com>
2023-05-26 16:07:33 +0200

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@ require "open-uri"
 
 file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
 article = Article.new(title: "NES", body: "A great console")
-article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+article.photos.attach(io: file, filename: "nes.png", content_type: "image/png")
 article.save
